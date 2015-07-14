c3be0b63e96c8041c321e0093d9d3693984f0f9e
Mark Chavez <mccd732@gmail.com>
2015-07-14 16:06:31 -0700

    adding and commiting but without pushing

--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -1,2 +1,3 @@
 class Bicycle < ActiveRecord::Base
+  validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   resources :bicycles
   root "bicycles#index"
+
+
+# WOOHOO CUSTOM ROUTE
+get '/bicycles/similar', to: 'bicycles#edit'
+
+
+# get '/sales/:guid', to: 'sales#show'
+
+
 end
